73206334973e29182ae1757c963013e9675e1b23
Unknown <MartinSStewart@gmail.com>
2021-04-14 23:14:37 +0200

    Temporarily ignore login

--- src/Backend.elm
+++ src/Backend.elm
@@ -412,11 +412,10 @@ update msg model =
 
         ClientConnected sessionId clientId ->
             ( model
-            , if Set.member sessionId model.clients then
-                sendUpdates clientId model
-
-              else
-                Cmd.none
+            , --if Set.member sessionId model.clients then
+              sendUpdates clientId model
+              --else
+              --Cmd.none
             )
 
         ClientDisconnected _ clientId ->
