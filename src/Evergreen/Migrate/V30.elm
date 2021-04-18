dfd001ed90f3b51001cdcef03e294b49d1d5ae33
Martin Stewart <MartinSStewart@gmail.com>
2021-04-18 19:28:56 +0200

    Remove vendored code again and add migration

---
+++ src/Evergreen/Migrate/V30.elm
@@ -0,0 +1,123 @@
+module Evergreen.Migrate.V30 exposing (..)
+
+import AssocList
+import Dict
+import Evergreen.V21.PackageStatus as OldPackageStatus
+import Evergreen.V21.Types as Old
+import Evergreen.V30.PackageStatus as NewPackageStatus
+import Evergreen.V30.Types as New
+import Lamdera.Migrations exposing (..)
+import List.Nonempty
+
+
+frontendModel : Old.FrontendModel -> ModelMigration New.FrontendModel New.FrontendMsg
+frontendModel old =
+    ModelUnchanged
+
+
+migrateRunRuleResult : OldPackageStatus.RunRuleResult -> NewPackageStatus.RunRuleResult
+migrateRunRuleResult ruleResult =
+    case ruleResult of
+        OldPackageStatus.ParsingError ->
+            NewPackageStatus.ParsingError (List.Nonempty.fromElement "")
+
+        OldPackageStatus.IncorrectProject ->
+            NewPackageStatus.IncorrectProject
+
+        OldPackageStatus.FixFailed problem ->
+            NewPackageStatus.FixFailed problem
+
+        OldPackageStatus.NotEnoughIterations ->
+            NewPackageStatus.NotEnoughIterations
+
+        OldPackageStatus.NotAnElm19xPackage ->
+            NewPackageStatus.NotAnElm19xPackage
+
+        OldPackageStatus.DependenciesDontExist nonempty ->
+            NewPackageStatus.DependenciesDontExist nonempty
+
+        OldPackageStatus.FoundErrors foundErrors_ ->
+            NewPackageStatus.FoundErrors foundErrors_
+
+        OldPackageStatus.NoErrorsFounds ->
+            NewPackageStatus.NoErrorsFounds
+
+
+migrateReviewResult : OldPackageStatus.ReviewResult -> NewPackageStatus.ReviewResult
+migrateReviewResult reviewResult =
+    case reviewResult of
+        OldPackageStatus.CouldNotOpenDefaultBranchZip ->
+            NewPackageStatus.CouldNotOpenDefaultBranchZip
+
+        OldPackageStatus.PackageTagNotFound ->
+            NewPackageStatus.PackageTagNotFound
+
+        OldPackageStatus.HttpError error ->
+            NewPackageStatus.HttpError error
+
+        OldPackageStatus.RuleErrors runRuleResult ->
+            NewPackageStatus.RuleErrors (migrateRunRuleResult runRuleResult)
+
+
+migrationPackageStatus : OldPackageStatus.PackageStatus -> NewPackageStatus.PackageStatus
+migrationPackageStatus packageStatus =
+    case packageStatus of
+        OldPackageStatus.Pending version updateIndex ->
+            NewPackageStatus.Pending version updateIndex
+
+        OldPackageStatus.Fetched record ->
+            NewPackageStatus.Fetched record
+
+        OldPackageStatus.FetchedAndChecked record ->
+            NewPackageStatus.FetchedAndChecked
+                { updateIndex = record.updateIndex
+                , docs = record.docs
+                , elmJson = record.elmJson
+                , result = migrateReviewResult record.result
+                }
+
+        OldPackageStatus.FetchingElmJsonAndDocsFailed version updateIndex _ ->
+            NewPackageStatus.Pending version updateIndex
+
+        OldPackageStatus.FetchedCheckedAndPullRequestPending fetchedAndChecked_ ->
+            NewPackageStatus.FetchedCheckedAndPullRequestPending fetchedAndChecked_
+
+        OldPackageStatus.FetchedCheckedAndPullRequestSent fetchedAndChecked_ record ->
+            NewPackageStatus.FetchedCheckedAndPullRequestSent fetchedAndChecked_ record
+
+        OldPackageStatus.FetchedCheckedAndPullRequestFailed fetchedAndChecked_ error ->
+            NewPackageStatus.FetchedCheckedAndPullRequestFailed fetchedAndChecked_ error
+
+
+backendModel : Old.BackendModel -> ModelMigration New.BackendModel New.BackendMsg
+backendModel old =
+    ModelMigrated
+        ( { cachedPackages =
+                Dict.map
+                    (\_ versions -> AssocList.map (\_ packageStatus -> migrationPackageStatus packageStatus) versions)
+                    old.cachedPackages
+          , clients = old.clients
+          , updateIndex = old.updateIndex
+          }
+        , Cmd.none
+        )
+
+
+frontendMsg : Old.FrontendMsg -> MsgMigration New.FrontendMsg New.FrontendMsg
+frontendMsg old =
+    MsgUnchanged
+
+
+toBackend : Old.ToBackend -> MsgMigration New.ToBackend New.BackendMsg
+toBackend old =
+    MsgUnchanged
+
+
+backendMsg : Old.BackendMsg -> MsgMigration New.BackendMsg New.BackendMsg
+backendMsg old =
+    MsgUnchanged
+
+
+toFrontend : Old.ToFrontend -> MsgMigration New.ToFrontend New.FrontendMsg
+toFrontend old =
+    MsgUnchanged
---
+++ src/Evergreen/V30/Types.elm
@@ -0,0 +1,124 @@
+module Evergreen.V30.Types exposing (..)
+
+import AssocList
+import Browser
+import Browser.Navigation
+import Dict
+import Elm.Docs
+import Elm.Package
+import Elm.Project
+import Elm.Version
+import Evergreen.V30.PackageStatus
+import Http
+import Lamdera
+import Set
+import Url
+
+
+type PackageStatusFrontend
+    = Fetched_
+        { version : Elm.Version.Version
+        , updateIndex : Int
+        }
+    | FetchedAndChecked_
+        { version : Elm.Version.Version
+        , updateIndex : Int
+        , result : Evergreen.V30.PackageStatus.ReviewResult
+        }
+    | FetchingElmJsonAndDocsFailed_ Elm.Version.Version Int Http.Error
+    | FetchedCheckedAndPullRequestPending_
+        { version : Elm.Version.Version
+        , updateIndex : Int
+        }
+    | FetchedCheckedAndPullRequestSent_
+        { version : Elm.Version.Version
+        , updateIndex : Int
+        , url : String
+        }
+    | FetchedCheckedAndPullRequestFailed_
+        { version : Elm.Version.Version
+        , updateIndex : Int
+        , error : Http.Error
+        }
+
+
+type DisplayOrder
+    = RequestOrder
+    | Alphabetical
+
+
+type LoginStatus
+    = NotLoggedIn String
+    | LoggedIn
+
+
+type alias FrontendModel =
+    { state : Dict.Dict String (List PackageStatusFrontend)
+    , key : Browser.Navigation.Key
+    , order : DisplayOrder
+    , loginStatus : LoginStatus
+    }
+
+
+type alias BackendModel =
+    { cachedPackages : Dict.Dict String (AssocList.Dict Elm.Version.Version Evergreen.V30.PackageStatus.PackageStatus)
+    , clients : Set.Set String
+    , updateIndex : Int
+    }
+
+
+type FrontendMsg
+    = UrlClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+    | ToggleOrder
+    | PressedResetBackend
+    | PressedResetRules
+    | NoOpFrontendMsg
+    | PressedLogin
+    | TypedPassword String
+    | PressedCreatePullRequest Elm.Package.Name
+    | PressedRerunPackage Elm.Package.Name Elm.Version.Version
+
+
+type ToBackend
+    = ResetBackend
+    | ResetRules
+    | LoginRequest String
+    | PullRequestRequest Elm.Package.Name
+    | RerunPackageRequest Elm.Package.Name Elm.Version.Version
+
+
+type BackendMsg
+    = GotNewPackagePreviews (Result Http.Error (List ( String, Elm.Version.Version )))
+    | FetchedElmJsonAndDocs
+        { packageName : Elm.Package.Name
+        , version : Elm.Version.Version
+        }
+        (Result Http.Error ( Elm.Project.Project, List Elm.Docs.Module ))
+    | RanNoUnused
+        { packageName : Elm.Package.Name
+        , elmJson : Elm.Project.PackageInfo
+        , docs : List Elm.Docs.Module
+        }
+        Evergreen.V30.PackageStatus.ReviewResult
+    | ClientConnected Lamdera.SessionId Lamdera.ClientId
+    | ClientDisconnected Lamdera.SessionId Lamdera.ClientId
+    | CreatePullRequestResult
+        Elm.Package.Name
+        Elm.Version.Version
+        (Result
+            Http.Error
+            { url : String
+            }
+        )
+    | ReranPackage
+        { packageName : Elm.Package.Name
+        , elmJson : Elm.Project.PackageInfo
+        , docs : List Elm.Docs.Module
+        , updateIndex : Int
+        }
+        Evergreen.V30.PackageStatus.ReviewResult
+
+
+type ToFrontend
+    = Updates (Dict.Dict String (List PackageStatusFrontend))
